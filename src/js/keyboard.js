393a8be30ef6205f877b49e8446ad79043c2a3f5
Yugeta Koji <yugeta@myntinc.com>
2023-08-13 09:07:54 +0900

    bug-fix : editor/flat/position

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -7,9 +7,13 @@ export class Keyboard{
     this.options = options || {}
     this.asset_load()
   }
+
+  // 鍵盤HTMLのファイルパス
   get filepath_asset(){
     return 'asset/octave.html'
   }
+
+  // 鍵盤HTMLの読み込み処理
   asset_load(){
     const xhr = new XMLHttpRequest()
     xhr.withCredentials = true;
@@ -18,28 +22,42 @@ export class Keyboard{
     xhr.onload = this.asset_loaded.bind(this)
     xhr.send()
   }
+
+  // 鍵盤HTMLの読み込み完了処理
   asset_loaded(e){
     this.asset_octave = e.target.response
     this.view_octave()
   }
+
+  // オクターブ別表示
   view_octave(){
     for(let i=0; i<Element.octave_count; i++){
       const html = new Convert(this.asset_octave).double_bracket({octave : i})
       Element.elm_keyboard.insertAdjacentHTML('afterbegin' ,html)
     }
     this.set_event()
+    this.set_center()
     this.finish()
   }
+
+  // イベントセット
   set_event(){
     Element.elm_keyboard.addEventListener('click' , Keyboard.key_click)
   }
 
+  // 初期位置をオクターブ中心にする
+  set_center(){
+    Element.elm_keyboard.scrollTop = (Element.elm_keyboard.scrollHeight - Element.elm_keyboard.offsetHeight) / 2
+  }
+
+  // 初期設定終了後処理
   finish(){
     if(this.options.callback){
       this.options.callback()
     }
   }
 
+  // 鍵盤をクリックした時の処理
   static key_click(e){
     const elm_oct = e.target.closest('.octave')
     const elm_key = e.target.closest('[data-key]')
