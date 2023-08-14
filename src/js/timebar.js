7118470e2a042ccc040f4629c07b44b9b66c7310
Yugeta Koji <yugeta@myntinc.com>
2023-08-14 10:14:38 +0900

    timebar : line

--- src/js/timebar.js
+++ src/js/timebar.js
@@ -1,8 +1,10 @@
-import { Event } from './event.js'
+import { Event }   from './event.js'
+import { Element } from './element.js'
 
 export class Timebar{
   constructor(){
     this.set_event()
+    this.view_line()
   }
 
   static get elm_timebar_area(){
@@ -14,17 +16,34 @@ export class Timebar{
   static get elm_timebar_icon(){
     return document.querySelector(`.timebar`)
   }
+  static get elm_timebar_line(){
+    return Element.elm_editor.querySelector(`:scope > .line`)
+  }
 
   set_event(){
     // scroll
     Timebar.elm_timebar_area.addEventListener('scroll', Event.scroll_sync_timeline)
     // drag
-    window.addEventListener('mousedown' , Timebar.mousedown)
-    window.addEventListener('mousemove' , Timebar.mousemove)
-    window.addEventListener('mouseup'   , Timebar.mouseup)
+    window.addEventListener('mousedown' , this.mousedown)
+    window.addEventListener('mousemove' , this.mousemove.bind(this))
+    window.addEventListener('mouseup'   , this.mouseup)
   }
 
-  static mousedown(e){
+  // 縦棒ライン
+  view_line(){
+    const line = document.createElement('div')
+    line.classList.add('line')
+    const height = Element.elm_editor.scrollHeight
+    line.style.setProperty('height',`${height}px`,'')
+    Element.elm_editor.appendChild(line)
+  }
+
+  // ラインがtimebarに追従する処理
+  follow_line(left){
+    Timebar.elm_timebar_line.style.setProperty('left',`${left}px`,'')
+  }
+
+  mousedown(e){
     const elm = e.target.closest('.timebar')
     if(!elm){return}
     const left = elm.style.getPropertyValue('left')
@@ -33,13 +52,14 @@ export class Timebar{
       left    : left ? Number(left.replace('px','')) : 0
     }
   }
-  static mousemove(e){
+  mousemove(e){
     if(!Timebar.click_data){return}
-    let x = Timebar.click_data.left + (e.pageX - Timebar.click_data.mouse_x)
-    x = x > 0 ? x : 0
-    Timebar.elm_timebar_icon.style.setProperty('left',`${x}px`,'')
+    let left = Timebar.click_data.left + (e.pageX - Timebar.click_data.mouse_x)
+    left = left > 0 ? left : 0
+    Timebar.elm_timebar_icon.style.setProperty('left',`${left}px`,'')
+    this.follow_line(left)
   }
-  static mouseup(e){
+  mouseup(e){
     if(!Timebar.click_data){return}
     delete Timebar.click_data
   }
@@ -47,4 +67,5 @@ export class Timebar{
   static set_width(size){
     Timebar.elm_timebar_scroll.style.setProperty('width',`${size}px`,'')
   }
+
 }
